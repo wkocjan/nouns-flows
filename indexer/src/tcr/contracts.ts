daab7530dcbaa890b42b38432f634ee81f0caa49
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-10-18 06:01:02 -0400

    fix ingestion

--- indexer/src/tcr/contracts.ts
+++ indexer/src/tcr/contracts.ts
@@ -1,4 +1,5 @@
 import { ponder } from "@/generated"
+import { rewardPoolImplAbi } from "../../abis"
 
 ponder.on("NounsFlowTcrFactory:FlowTCRDeployed", async (params) => {
   const { event, context } = params
@@ -13,14 +14,29 @@ ponder.on("NounsFlowTcrFactory:FlowTCRDeployed", async (params) => {
     rewardPoolProxy,
   } = event.args
 
+  const [superToken, managerRewardSuperfluidPool] = await Promise.all([
+    context.client.readContract({
+      address: flowProxy,
+      abi: context.contracts.NounsFlow.abi,
+      functionName: "superToken",
+    }),
+    context.client.readContract({
+      address: rewardPoolProxy,
+      abi: rewardPoolImplAbi,
+      functionName: "rewardPool",
+    }),
+  ])
+
   await Grant.updateMany({
     where: { recipient: flowProxy.toLowerCase() },
     data: {
+      superToken: superToken.toLowerCase(),
       tcr: flowTCRProxy.toLowerCase(),
       arbitrator: arbitratorProxy.toLowerCase(),
       erc20: erc20Proxy.toLowerCase(),
       tokenEmitter: tokenEmitterProxy.toLowerCase(),
       managerRewardPool: rewardPoolProxy.toLowerCase(),
+      managerRewardSuperfluidPool: managerRewardSuperfluidPool.toLowerCase(),
     },
   })
 })
