5586bfd59501fd12b120a22f982d7f4ff028b51b
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-10-25 04:00:31 -0400

    hide apply if voting active!

--- web/app/flow/[flowId]/components/cta-buttons.tsx
+++ web/app/flow/[flowId]/components/cta-buttons.tsx
@@ -5,18 +5,22 @@ import Link from "next/link"
 import { VotingToggle } from "./voting-toggle"
 import { useAccount } from "wagmi"
 import { useVotingPower } from "@/lib/voting/use-voting-power"
+import { useVoting } from "@/lib/voting/voting-context"
 
 export function CTAButtons() {
   const { isConnected } = useAccount()
   const { votingPower } = useVotingPower()
+  const { isActive } = useVoting()
 
   const showVotingToggle = isConnected && votingPower > 0
 
   return (
     <div className="flex items-center space-x-4">
-      <Button variant={showVotingToggle ? "outline" : "default"}>
-        <Link href={`/apply`}>Apply</Link>
-      </Button>
+      {!isActive && (
+        <Button variant={showVotingToggle ? "outline" : "default"}>
+          <Link href={`/apply`}>Apply</Link>
+        </Button>
+      )}
       {showVotingToggle && <VotingToggle />}
     </div>
   )
