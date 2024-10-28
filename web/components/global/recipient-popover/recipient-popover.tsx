78f414da10c1ce6d679fa0469739382e7871c10e
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-10-28 17:22:57 -0400

    fix

--- web/components/global/recipient-popover/recipient-popover.tsx
+++ web/components/global/recipient-popover/recipient-popover.tsx
@@ -26,8 +26,10 @@ export const RecipientPopover = () => {
   const { data, isLoading } = useServerFunction(getUserUpdatesChannel, "updates-channel", [address])
 
   useEffect(() => {
-    setIsVisible(!!address && grants.length > 0)
-  }, [address, grants])
+    setIsVisible(!!address)
+  }, [address])
+
+  const hasGrants = grants.length > 0
 
   if (!isVisible) return null
 
@@ -65,76 +67,88 @@ export const RecipientPopover = () => {
               </a>
             )}
           </div>
-          <div className="mt-6">
-            <div className="mb-2 grid grid-cols-4 gap-2 text-xs font-medium text-muted-foreground">
-              <div className="col-start-3 text-center">Earned</div>
-              <div className="text-center">Claimable</div>
-            </div>
-            {grants.map((grant) => (
-              <div
-                key={grant.id}
-                className="grid grid-cols-4 items-center gap-2 border-t border-border py-2"
-              >
-                <div className="col-span-2 flex items-center space-x-2 overflow-hidden">
-                  <Image
-                    src={getIpfsUrl(grant.image)}
-                    alt={grant.title}
-                    className="size-6 flex-shrink-0 rounded-full object-cover"
-                    width={24}
-                    height={24}
-                  />
-                  <Link
-                    href={`/item/${grant.id}`}
-                    className="truncate text-sm hover:underline"
-                    onClick={() => closeRef.current?.click()}
+          {hasGrants ? (
+            <>
+              <div className="mt-6">
+                <div className="mb-2 grid grid-cols-4 gap-2 text-xs font-medium text-muted-foreground">
+                  <div className="col-start-3 text-center">Earned</div>
+                  <div className="text-center">Claimable</div>
+                </div>
+                {grants.map((grant) => (
+                  <div
+                    key={grant.id}
+                    className="grid grid-cols-4 items-center gap-2 border-t border-border py-2"
                   >
-                    {grant.title}
-                  </Link>
-                </div>
-                <Currency as="div" className="text-center text-sm font-medium">
-                  {grant.totalEarned}
-                </Currency>
-                <div className="flex items-center justify-center">
-                  <WithdrawSalaryButton
-                    onSuccess={refetch}
-                    flow={getEthAddress(grant.parentContract)}
-                    pools={[grant.flow.baselinePool, grant.flow.bonusPool].map((pool) =>
-                      getEthAddress(pool),
-                    )}
-                  />
-                </div>
+                    <div className="col-span-2 flex items-center space-x-2 overflow-hidden">
+                      <Image
+                        src={getIpfsUrl(grant.image)}
+                        alt={grant.title}
+                        className="size-6 flex-shrink-0 rounded-full object-cover"
+                        width={24}
+                        height={24}
+                      />
+                      <Link
+                        href={`/item/${grant.id}`}
+                        className="truncate text-sm hover:underline"
+                        onClick={() => closeRef.current?.click()}
+                      >
+                        {grant.title}
+                      </Link>
+                    </div>
+                    <Currency as="div" className="text-center text-sm font-medium">
+                      {grant.totalEarned}
+                    </Currency>
+                    <div className="flex items-center justify-center">
+                      <WithdrawSalaryButton
+                        onSuccess={refetch}
+                        flow={getEthAddress(grant.parentContract)}
+                        pools={[grant.flow.baselinePool, grant.flow.bonusPool].map((pool) =>
+                          getEthAddress(pool),
+                        )}
+                      />
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
-          </div>
-          {needsVerify && (
-            <Alert className="mt-6" variant="default">
-              <AlertDescription>
-                Please connect your wallet address ({getShortEthAddress(address)}) with Farcaster
-                account.
-                <br />
-                <Button asChild size="sm" variant="default" className="mt-2">
-                  <a href="https://warpcast.com/~/settings/verified-addresses" target="_blank">
-                    Verify on Farcaster
-                  </a>
-                </Button>
-              </AlertDescription>
-            </Alert>
-          )}
-          {shouldJoinFlowsChannel && (
-            <Alert className="mt-6" variant="default">
-              <AlertDescription>
-                Please join the Flows channel on Farcaster to post updates about your work.
-                <br />
-                <Button asChild size="sm" variant="default" className="mt-2">
-                  <a
-                    href="https://warpcast.com/~/channel/flows/join?inviteCode=35EHtdIhE-ivqVxl2SaEFg"
-                    target="_blank"
-                  >
-                    Join /flows
-                  </a>
-                </Button>
-              </AlertDescription>
-            </Alert>
+
+              {needsVerify && (
+                <Alert className="mt-6" variant="default">
+                  <AlertDescription>
+                    Please connect your wallet address ({getShortEthAddress(address)}) with
+                    Farcaster account.
+                    <br />
+                    <Button asChild size="sm" variant="default" className="mt-2">
+                      <a href="https://warpcast.com/~/settings/verified-addresses" target="_blank">
+                        Verify on Farcaster
+                      </a>
+                    </Button>
+                  </AlertDescription>
+                </Alert>
+              )}
+              {shouldJoinFlowsChannel && (
+                <Alert className="mt-6" variant="default">
+                  <AlertDescription>
+                    Please join the Flows channel on Farcaster to post updates about your work.
+                    <br />
+                    <Button asChild size="sm" variant="default" className="mt-2">
+                      <a
+                        href="https://warpcast.com/~/channel/flows/join?inviteCode=35EHtdIhE-ivqVxl2SaEFg"
+                        target="_blank"
+                      >
+                        Join /flows
+                      </a>
+                    </Button>
+                  </AlertDescription>
+                </Alert>
+              )}
+            </>
+          ) : (
+            <div className="mt-8 flex flex-col items-center justify-center space-x-2 space-y-4 rounded-xl border border-border bg-gray-200/30 py-6 text-sm text-muted-foreground dark:bg-gray-800">
+              <Button size="lg" asChild>
+                <Link href="/apply">Apply now</Link>
+              </Button>
+              <p className="px-2">Apply for a grant today to start earning.</p>
+            </div>
           )}
         </div>
       </PopoverContent>
