781f850acdfc0205474c1e780ab452f17cbd0559
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-11-01 02:32:52 -0400

    standardize data

--- web/app/crons/derived-data/template/route.ts
+++ web/app/crons/derived-data/template/route.ts
@@ -11,14 +11,26 @@ export const maxDuration = 300
 export async function GET() {
   try {
     const flows = await database.grant.findMany({
-      where: { isFlow: 1, isTopLevel: 0, template: null },
+      where: {
+        isFlow: 1,
+        isTopLevel: 0,
+        OR: [{ derivedData: null }, { derivedData: { template: null } }],
+      },
     })
 
     for (const flow of flows) {
       const { template } = await generateTemplate(flow.title, flow.description)
       console.debug({ title: flow.title, template })
-
-      await database.template.create({ data: { content: template, grantId: flow.id } })
+      await database.derivedData.upsert({
+        where: { grantId: flow.id },
+        update: { template, updatedAt: new Date() },
+        create: {
+          template,
+          grantId: flow.id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      })
 
       await new Promise((resolve) => setTimeout(resolve, 500))
     }
