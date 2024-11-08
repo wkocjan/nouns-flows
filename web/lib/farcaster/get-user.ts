6856445ca4a74acf4132b7e2cc7dfee6e56052c5
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-11-07 19:01:14 -0500

    ttl

--- web/lib/farcaster/get-user.ts
+++ web/lib/farcaster/get-user.ts
@@ -1,42 +1,33 @@
-import { unstable_cache } from "next/cache"
 import { farcasterDb } from "@/lib/database/farcaster"
 
-export const getFarcasterUserByEthAddress = unstable_cache(
-  async (address: `0x${string}`) => {
-    try {
-      const users = await getFarcasterUsersByEthAddress(address)
-      if (!users || users.length === 0) return null
+export const getFarcasterUserByEthAddress = async (address: `0x${string}`) => {
+  try {
+    const users = await getFarcasterUsersByEthAddress(address)
+    if (!users || users.length === 0) return null
 
-      return users[0]
-    } catch (e: any) {
-      console.error(e?.message)
-      return null
-    }
-  },
-  undefined,
-  { revalidate: 600 }, // 10 minutes
-)
+    return users[0]
+  } catch (e: any) {
+    console.error(e?.message)
+    return null
+  }
+}
 
-export const getFarcasterUsersByEthAddress = unstable_cache(
-  async (rawAddress: `0x${string}`) => {
-    try {
-      const address = rawAddress.toLowerCase()
+export const getFarcasterUsersByEthAddress = async (rawAddress: `0x${string}`) => {
+  try {
+    const address = rawAddress.toLowerCase()
 
-      const users = await farcasterDb.farcasterProfile.findMany({
-        where: {
-          verified_addresses: {
-            has: address,
-          },
+    const users = await farcasterDb.farcasterProfile.findMany({
+      where: {
+        verified_addresses: {
+          has: address,
         },
-        cacheStrategy: { ttl: 600 },
-      })
+      },
+      cacheStrategy: { ttl: 600 },
+    })
 
-      return users
-    } catch (e: any) {
-      console.error(e?.message)
-      return []
-    }
-  },
-  undefined,
-  { revalidate: 3600 }, // 1 hour
-)
+    return users
+  } catch (e: any) {
+    console.error(e?.message)
+    return []
+  }
+}
