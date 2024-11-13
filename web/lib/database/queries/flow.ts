b945c3d71638cb802e8ab1b4f2f22d92b4155c9c
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-11-13 12:37:00 -0500

    cache and flowid layout edge

--- web/lib/database/queries/flow.ts
+++ web/lib/database/queries/flow.ts
@@ -1,16 +1,18 @@
-import database from "@/lib/database"
+import database from "@/lib/database/edge"
 import { cache } from "react"
 
 export const getFlowWithGrants = cache(async (flowId: string) => {
   return await database.grant.findFirstOrThrow({
     where: { id: flowId, isActive: 1 },
     include: { subgrants: true, derivedData: true },
+    cacheStrategy: { swr: 120 },
   })
 })
 
 export const getFlow = cache(async (flowId: string) => {
   return await database.grant.findFirstOrThrow({
     where: { id: flowId, isFlow: 1, isActive: 1 },
+    cacheStrategy: { swr: 120 },
   })
 })
 
