449af2e9130710a9dcdaab8a92c5f47337d1b22d
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-11-13 12:35:45 -0500

    revert

--- web/lib/auth/user.ts
+++ web/lib/auth/user.ts
@@ -12,28 +12,15 @@ export type User = {
 }
 
 export async function getUser() {
-  console.time("getUserAddressFromCookie")
   const address = await getUserAddressFromCookie()
-  console.timeEnd("getUserAddressFromCookie")
-
   if (!address) return undefined
 
-  console.time("getFarcasterUser")
   const farcasterUser = await getFarcasterUserByEthAddress(address)
-  console.timeEnd("getFarcasterUser")
-
-  console.time("getUsername")
-  const username =
-    farcasterUser?.fname || (await getEnsNameFromAddress(address)) || getShortEthAddress(address)
-  console.timeEnd("getUsername")
-
-  console.time("getAvatar")
-  const avatar = farcasterUser?.avatar_url || (await getEnsAvatar(address)) || undefined
-  console.timeEnd("getAvatar")
 
   return {
     address,
-    username,
-    avatar,
+    username:
+      farcasterUser?.fname || (await getEnsNameFromAddress(address)) || getShortEthAddress(address),
+    avatar: farcasterUser?.avatar_url || (await getEnsAvatar(address)) || undefined,
   } satisfies User
 }
