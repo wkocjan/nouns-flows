17d7e3c029e218591c335dd2f2e5da69ef8dc36f
Wojciech Kocjan <wojciech@kocjan.net>
2024-11-25 16:23:17 +0100

    Migrate next config to ts

---
+++ web/next.config.ts
@@ -0,0 +1,26 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  experimental: {
+    after: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "media.giphy.com", pathname: "/**" },
+      { protocol: "https", hostname: "*.mypinata.cloud", pathname: "/**" },
+      { protocol: "https", hostname: "ipfs.io", pathname: "/**" },
+      { protocol: "https", hostname: "noun.pics", pathname: "/**" },
+      { protocol: "https", hostname: "imagedelivery.net", pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "dmo9tcngmx442k9p.public.blob.vercel-storage.com",
+        pathname: "/**",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
+
+export default nextConfig
