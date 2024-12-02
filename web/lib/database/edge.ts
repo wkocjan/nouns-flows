ef9bfacee0ef0e03db954356ab9ff38f553d8fd6
rocketman <20303031+rocketman-21@users.noreply.github.com>
2024-12-02 12:43:21 -0500

    try to find slow queries

--- web/lib/database/edge.ts
+++ web/lib/database/edge.ts
@@ -1,12 +1,19 @@
 import { PrismaClient as PrismaClientEdge } from "@prisma/flows/edge"
 import { PrismaClient as PrismaClientLocal } from "@prisma/flows"
-
 import { withAccelerate } from "@prisma/extension-accelerate"
+import { withOptimize } from "@prisma/extension-optimize"
 
 const isDevelopment = process.env.NODE_ENV !== "production"
+const optimizeApiKey = process.env.PRISMA_OPTIMIZE_KEY
+
+if (!optimizeApiKey) {
+  throw new Error("PRISMA_OPTIMIZE_KEY is not set")
+}
 
 const getAccelerateClient = () => {
-  return new PrismaClientEdge().$extends(withAccelerate())
+  return new PrismaClientEdge()
+    .$extends(withAccelerate())
+    .$extends(withOptimize({ apiKey: optimizeApiKey }))
 }
 
 const prismaClientSingleton = () => {
