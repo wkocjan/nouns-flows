1a9e62da28379af9a54504ba1dc8c434e2868b20
Wojciech Kocjan <wojciech@kocjan.net>
2024-12-02 21:03:09 +0100

    Improve new grant page

--- web/app/crons/derived-data/grant-page/route.ts
+++ web/app/crons/derived-data/grant-page/route.ts
@@ -1,8 +1,8 @@
 import "server-only"
 
+import { generateAndStoreGrantPageData } from "@/app/item/[grantId]/page-data/get"
 import database from "@/lib/database/edge"
 import { NextResponse } from "next/server"
-import { generateAndStoreGrantPageData } from "@/app/item/[grantId]/page-data/get"
 
 export const maxDuration = 300
 
@@ -12,9 +12,9 @@ export async function GET() {
       where: {
         isFlow: 0,
         isTopLevel: 0,
-        // OR: [{ derivedData: null }, { derivedData: { pageData: null } }],
-        id: "0x1156a5456bd96493aebc28b9d022b6249edc73a19be1534a62ab78d6e527db5d",
+        OR: [{ derivedData: null }, { derivedData: { pageData: null } }],
       },
+      take: 2,
     })
 
     for (const grant of grants) {
