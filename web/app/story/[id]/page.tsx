a17f40723f91008935077c4e33b6d9a90de109d6
rocketman <20303031+rocketman-21@users.noreply.github.com>
2024-12-05 14:56:41 -0300

    Update page.tsx

--- web/app/story/[id]/page.tsx
+++ web/app/story/[id]/page.tsx
@@ -221,7 +221,7 @@ export default async function Page(props: Props) {
                         alt={grant.title}
                         width={40}
                         height={40}
-                        className="size-[40px] rounded-full"
+                        className="size-[40px] rounded-full object-cover"
                       />
                     )}
                     <div className="flex flex-col space-y-1">
