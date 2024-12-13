1ccdb1d2dc5adf7b4d416ef4a7fbb0ad54ed5273
Wojciech Kocjan <wojciech@kocjan.net>
2024-12-13 17:24:15 +0100

    damn

--- web/app/item/[grantId]/components/get-voters.ts
+++ web/app/item/[grantId]/components/get-voters.ts
@@ -6,7 +6,7 @@ export async function getVoters(contract: `0x${string}`, recipientId: string) {
   const voters = await database.$queryRaw<{ voter: `0x${string}`; votes_count: BigInt }[]>`
          SELECT voter, SUM(CAST("votes_count" AS INTEGER)) as "votes_count"
          FROM "Vote"
-         WHERE "contract" = ${contract} AND "recipientId" = ${recipientId} AND "isStale" = 0
+         WHERE "contract" = ${contract} AND "recipient_id" = ${recipientId} AND "is_stale" = 0
          GROUP BY voter    
         `
 
