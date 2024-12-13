df8883f85340e9e5018412db9eb6b86d1f524077
Wojciech Kocjan <wojciech@kocjan.net>
2024-12-13 17:16:49 +0100

    Fix column name in raw query

--- web/app/item/[grantId]/components/get-voters.ts
+++ web/app/item/[grantId]/components/get-voters.ts
@@ -3,12 +3,12 @@ import "server-only"
 import database from "@/lib/database/edge"
 
 export async function getVoters(contract: `0x${string}`, recipientId: string) {
-  const voters = await database.$queryRaw<{ voter: `0x${string}`; votesCount: BigInt }[]>`
-         SELECT voter, SUM(CAST("votesCount" AS INTEGER)) as "votesCount"
+  const voters = await database.$queryRaw<{ voter: `0x${string}`; votes_count: BigInt }[]>`
+         SELECT voter, SUM(CAST("votes_count" AS INTEGER)) as "votes_count"
          FROM "Vote"
          WHERE "contract" = ${contract} AND "recipientId" = ${recipientId} AND "isStale" = 0
          GROUP BY voter    
         `
 
-  return voters.map((v) => ({ ...v, votesCount: v.votesCount.toString() }))
+  return voters.map((v) => ({ ...v, votesCount: v.votes_count.toString() }))
 }
