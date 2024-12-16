9c65b1d8cbc05f64551df718ff464dee525da344
rocketman <20303031+rocketman-21@users.noreply.github.com>
2024-12-16 11:02:27 -0500

    tmp

--- web/lib/database/edge.ts
+++ web/lib/database/edge.ts
@@ -1,6 +1,7 @@
 // this will be updated by the edge-prebuild.js script to pull in the edge client
 import { PrismaClient } from "@prisma/flows"
 import { withAccelerate } from "@prisma/extension-accelerate"
+// import { withOptimize } from "@prisma/extension-optimize"
 
 const isDevelopment = process.env.NODE_ENV !== "production"
 const optimizeApiKey = process.env.PRISMA_OPTIMIZE_KEY
@@ -16,6 +17,7 @@ const getAccelerateClient = () => {
 const prismaClientSingleton = () => {
   if (isDevelopment) {
     return new PrismaClient()
+    // .$extends(withOptimize({ apiKey: optimizeApiKey }))
   }
   return getAccelerateClient()
 }
