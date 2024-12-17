7bbcd25d5098b08a686405162c437c556c12eccc
Wojciech Kocjan <wojciech@kocjan.net>
2024-12-17 19:19:44 +0100

    Remove markdown editor (blocknote)

--- web/components/ui/markdown-input.tsx
+++ web/components/ui/markdown-input.tsx
@@ -1,57 +1,27 @@
 "use client"
 
-import { cn } from "@/lib/utils"
-import { Block } from "@blocknote/core"
-import dynamic from "next/dynamic"
 import { CSSProperties, useState } from "react"
+import { Textarea } from "./textarea"
 
 interface Props {
   name: string
-  initialBlocks?: Block[]
-  initialMarkdown?: string
+  initialValue?: string
   minHeight?: CSSProperties["minHeight"]
-  editable?: boolean
   className?: string
 }
 
-const MarkdownEditor = dynamic(
-  () => import("@/components/global/markdown-editor/markdown-editor"),
-  {
-    ssr: false,
-  },
-)
-
 export const MarkdownInput = (props: Props) => {
-  const {
-    name,
-    initialBlocks,
-    initialMarkdown,
-    editable = true,
-    minHeight = 320,
-    className = "",
-  } = props
-  const [blocks, setBlocks] = useState(initialBlocks)
-  const [markdown, setMarkdown] = useState(initialMarkdown)
+  const { name, initialValue, minHeight = 320, className = "" } = props
+  const [markdown, setMarkdown] = useState(initialValue)
 
   return (
-    <div
-      className={cn(
-        "rounded-md border border-input bg-transparent px-3 py-5 shadow-sm transition-colors focus-within:ring-1 focus-within:ring-ring",
-        className,
-      )}
+    <Textarea
+      name={name}
+      value={markdown}
+      onChange={(e) => setMarkdown(e.target.value)}
+      placeholder="Write markdown..."
+      className={className}
       style={{ minHeight }}
-    >
-      <MarkdownEditor
-        initialBlocks={initialBlocks}
-        initialMarkdown={initialMarkdown}
-        onUpdate={(blocks, markdown) => {
-          setBlocks(blocks)
-          setMarkdown(markdown)
-        }}
-        editable={editable}
-      />
-      <input type="hidden" name={`${name}Blocks`} value={JSON.stringify(blocks)} />
-      <input type="hidden" name={`${name}Markdown`} value={markdown} />
-    </div>
+    />
   )
 }
